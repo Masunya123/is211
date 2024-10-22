792d0dc3df2e982908e9fdc543ba0b46d0899bbc
Студент 8 Кабинет 409 <409-8@coopteh.local>
2024-10-22 17:17:20 +0700

    добавлен тест

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -11,4 +11,16 @@ class RouterTest extends TestCase {
         $pos= mb_strpos($html, "Создание заказа");
         $this->assertNotFalse( $pos>=0);
     }
+        public function test_router1() {
+            $router = new Router();
+            $html = $router->route( "http://localhost" );
+            $pos= mb_strpos($html, "Посетите наш магазин");
+            $this->assertNotFalse( $pos>=0);
+        }
+        public function test_router3() {
+            $router = new Router();
+            $html = $router->route( "http://localhost/products" );
+            $pos= mb_strpos($html, "Добавить в корзину");
+            $this->assertNotFalse( $pos>=0);
+    }
 }
